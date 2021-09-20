be1d83352815492774dfd441a5449be199848c4e
flacial <ruthemiumwithdiamond@protonmail.com>
2021-09-20 17:17:50 -0400

    chore: Move often used methods to Utils

---
+++ utils/utils.js
@@ -0,0 +1,12 @@
+import path from 'path';
+
+class Utils {
+    static filePath = (pathName) => `${path.resolve()}/public/${pathName}`;
+    static CORS_HEADERS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST',
+        'Access-Control-Allow-Headers': 'Content-Type, Origin',
+      }
+}
+
+export default Utils;
