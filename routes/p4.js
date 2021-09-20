237bc6f3244532be0a1ebea9bdcab4e62a92c820
flacial <ruthemiumwithdiamond@protonmail.com>
2021-09-20 17:20:45 -0400

    p4/feat: Init new AssetCreation class

--- routes/p4.js
+++ routes/p4.js
@@ -1,20 +1,14 @@
 import express from "express";
 import AssetCreation from '../controllers/p4Controller.js';
-import path from 'path';
-
-const dirname = path.resolve();
+import Utils from "../utils/utils.js";
 
 const p4Router = express.Router();
+const assetCreation = new AssetCreation(3 * 1000, './temp/')
+
 p4Router.use(express.static('public'));
-
-const filePath = (pathName) => `${dirname}/public/${pathName}`;
-
-p4Router.get("/", (req, res) => res.sendFile(filePath("p4.html")));
-
-p4Router.post("/api/files", AssetCreation.handleFileCreation);
-
-p4Router.get("/api/files", AssetCreation.handleFilesList);
-
-p4Router.get("/api/files/:name", AssetCreation.handleFileContent);
+p4Router.get("/", (_req, res) => res.sendFile(Utils.filePath("p4.html")));
+p4Router.post("/api/files", assetCreation.handleFileCreation);
+p4Router.get("/api/files", assetCreation.handleFilesList);
+p4Router.get("/api/files/:name", assetCreation.handleFileContent);
 
 export default p4Router;
