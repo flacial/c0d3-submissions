713d78ca4800eb4c00137a75ad647a11df85c3a6
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-07 14:10:11 -0400

    style: Format LRU

--- utils/lru-cache.js
+++ utils/lru-cache.js
@@ -2,32 +2,32 @@ class LRU_Cache {
     #cache;
 
     constructor(maxSize = 10) {
-        this.maxSize = maxSize
-        this.#cache = new Map()
+      this.maxSize = maxSize;
+      this.#cache = new Map();
     }
 
-
     addItem = (key, value) => {
-        if (this.#cache.has(key)) return this
+      if (this.#cache.has(key)) return this;
 
-        if (this.#cache.size === this.maxSize) this.#cache.delete(this.tail)
+      if (this.#cache.size === this.maxSize) this.#cache.delete(this.tail);
 
-        this.#cache.set(key, value)
-        return this
+      this.#cache.set(key, value);
+      return this;
     }
 
     // Used for jest testing.
-    get keys() { return this.#cache.keys() }
+    get keys() { return this.#cache.keys(); }
 
-    get tail() { return this.keys.next().value }
+    get tail() { return this.keys.next().value; }
 
     hasItem = (key) => this.#cache.has(key)
-    getItem = (key) => {
-        const cachedItem = this.#cache.get(key)
 
-        this.#cache.delete(key)
-        this.#cache.set(key, cachedItem)
-        return cachedItem
+    getItem = (key) => {
+      const cachedItem = this.#cache.get(key);
+
+      this.#cache.delete(key);
+      this.#cache.set(key, cachedItem);
+      return cachedItem;
     }
 }
 
