d69a5acc7eab1997bbcaeac565d849ace9da0689
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-25 11:16:28 -0400

    p7: Complete

---
+++ controllers/p7Controller.js
@@ -0,0 +1,108 @@
+import Tesseract from 'tesseract.js';
+import { v4 as uuid } from 'uuid';
+import fs from 'fs';
+import Utils from '../utils/utils.js';
+
+const fsP = fs.promises;
+const jobs = {};
+
+const deleteTrainedData = async () => {
+  const files = await fsP.readdir('./');
+
+  files.forEach((file) => {
+    if (file.includes('traineddata')) {
+      fsP.unlink(file);
+      console.log('Deleted training data:', file);
+    }
+  });
+};
+
+const createJobWorkers = (jobId, req) => {
+  jobs[jobId] = {};
+
+  req.files.forEach((file) => {
+    const { filename } = file;
+    const job = jobs[jobId];
+
+    job[filename] = { text: 'Processing the file', error: false, done: false };
+    const jobFile = job[filename];
+
+    Tesseract.recognize(Utils.filePath(`images/${filename}`), 'eng').then((d) => {
+      jobFile.text = d.data.text;
+      jobFile.done = true;
+    }).catch(() => {
+      jobFile.error = true;
+      jobFile.done = true;
+    });
+  });
+};
+
+const isJobDone = (job) => jobs[job] && Object.values(jobs[job]).every(({ done }) => done);
+const jobPath = (req, path, jobId) => `${req.protocol}://${req.headers.host}${path}${jobId}`;
+
+export const handleImageUpload = async (req, res) => {
+  try {
+    const jobId = uuid();
+
+    await deleteTrainedData();
+    createJobWorkers(jobId, req);
+
+    return res.status(201).send({
+      jobUrl: jobPath(req, '/p7/imageAnalysis/jobs/', jobId),
+    });
+  } catch (err) {
+    res.status(500).send({ error: { message: "Couldn't delete trained data" } });
+  }
+};
+
+export const handleJob = (req, res) => {
+  const { jobId } = req.params;
+  let html = "<h1>Job doesn't exist</h1>";
+
+  if (jobs[jobId]) {
+    html = `
+    <style>
+      * {
+        box-sizing: border-box;
+      }
+      
+      .jobContainer {
+        display: flex; 
+        column-gap: 40px; 
+        max-width: 600px; 
+        margin: 30px auto;
+      }
+
+      .jobImage {
+        max-width: 400
+      }
+    </style>
+    <h1>Job process: ${jobId}</h1>
+    <div>
+    ${Object.entries(jobs[jobId]).reduce((acc, [imageName, job]) => `${acc}<div class="jobContainer"><img class="jobImage" src="/images/${imageName}" alt="${imageName}"/><p>${job.error ? 'Error occured' : job.text}</p></div>`, '')}
+    </div>
+    <script>
+    const timeouts = [];
+    const getProcessUpdates = async () => {
+      const job = await fetch("${jobPath(req, '/p7/imageAnalysis/jobs/', jobId)}")
+
+      const jobHtml = await job.text()
+      document.body.innerHTML = jobHtml
+
+      const jobDone = await fetch("${jobPath(req, '/p7/imageAnalysis/jobs/state/', jobId)}")
+
+      const isJobDone = await jobDone.text();
+      if (isJobDone === "true") return timeouts.forEach((id) => clearTimeout(id))
+
+      timeouts.push(setTimeout(() => getProcessUpdates(), 2000))
+    }
+    
+    getProcessUpdates()
+    </script>
+    `;
+  }
+
+  res.send(html);
+};
+
+export const handleJobDone = (req, res) => res.send(isJobDone(req.params.jobId));
--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,13 @@
 import path from 'path';
 
 class Utils {
-    static filePath = (pathName) => `${path.resolve()}/public/${pathName}`;
+  static filePath = (pathName) => `${path.resolve()}/public/${pathName}`;
 
-    static CORS_HEADERS = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type, Origin, Authorization',
-    }
+  static CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Origin, Authorization',
+  }
 }
 
 export default Utils;
