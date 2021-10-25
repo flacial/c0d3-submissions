7f1ca1d8cc33bd67b82ab8e247ae2e2cf840feda
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-25 11:40:18 -0400

    p4: Format file

--- routes/p4.js
+++ routes/p4.js
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express';
 import AssetCreation from '../controllers/p4Controller.js';
-import Utils from "../utils/utils.js";
+import Utils from '../utils/utils.js';
 
 const p4Router = express.Router();
-const assetCreation = new AssetCreation(10 * 1000, './temp/')
+const assetCreation = new AssetCreation(10 * 1000, './temp/');
 
 p4Router.use(express.static('public'));
-p4Router.get("/", (_req, res) => res.sendFile(Utils.filePath("p4.html")));
-p4Router.post("/api/files", assetCreation.handleFileCreation);
-p4Router.get("/api/files", assetCreation.handleFilesList);
-p4Router.get("/api/files/:name", assetCreation.handleFileContent);
+p4Router.get('/', (_req, res) => res.sendFile(Utils.filePath('p4.html')));
+p4Router.post('/api/files', assetCreation.handleFileCreation);
+p4Router.get('/api/files', assetCreation.handleFilesList);
+p4Router.get('/api/files/:name', assetCreation.handleFileContent);
 
 export default p4Router;
