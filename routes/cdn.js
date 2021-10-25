451267737fbc4f310f2852959068b4f875d2806f
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-25 11:40:11 -0400

    cdn: Format file

--- routes/cdn.js
+++ routes/cdn.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import Utils from '../utils/utils.js'
+import Utils from '../utils/utils.js';
 
 const cdnRouter = express.Router();
 cdnRouter.use(express.static('cdn'));
 
-cdnRouter.get('/:file', (req, res) => {
-    const { file } = req.params;
-
-    return res.sendFile(Utils.filePath(file))
-})
+cdnRouter.get('/:file', (req, res) => res.sendFile(Utils.filePath(req.params.file)));
 
 export default cdnRouter;
