cb9143ebdcf18443f3006f3c5b02b0581603ae93
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-29 10:37:27 -0400

    utils: Create createDir
    create directories for each project

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 
 class Utils {
   static filePath = (pathName) => `${path.resolve()}/public/${pathName}`;
@@ -9,7 +10,11 @@ class Utils {
     'Access-Control-Allow-Headers': 'Content-Type, Origin, Authorization',
   }
 
-  static resolvePath = (req, dir) => `${req.protocol}://${req.headers.host}${dir}`;
+  static resolvePath = (req, dir, https) => `${https ? 'https' : req.protocol}://${req.headers.host}${dir}`;
+
+  static createDir = (dirPath) => {
+    if (!fs.existsSync(dirPath)) fs.mkdir(dirPath, (e) => e && console.error(e));
+  }
 }
 
 export default Utils;
