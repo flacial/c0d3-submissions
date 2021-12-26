e392d38f5903ee058c56ea1fc5261db06761106f
flacial <ruthemiumwithdiamond@protonmail.com>
2021-12-26 18:45:55 -0500

    Format files

--- routes/memeChat.js
+++ routes/memeChat.js
@@ -1,35 +1,35 @@
-import express from "express";
-import Utils from "../utils/utils.js";
+import express from 'express';
+import Utils from '../utils/utils.js';
 import {
   handleLogin,
   renderSecurePage,
   handleMemeCreation,
   handleSendingAllMemes,
   handleSendingMeme,
-} from "../controllers/memeChat-controller.js";
+} from '../controllers/memeChat-controller.js';
 
-Utils.createDir("public/p9-memes");
+Utils.createDir('public/p9-memes');
 
 const p9Router = express.Router();
 
 const renderLoginPage = renderSecurePage({
-  page: "login",
-  redirectTo: "/p9/memechat",
-  directTo: "p9-login.html",
+  page: 'login',
+  redirectTo: '/p9/memechat',
+  directTo: 'p9-login.html',
 });
 const renderMemechatPage = renderSecurePage({
-  page: "memechat",
-  redirectTo: "/p9/login",
-  directTo: "p9-memechat.html",
+  page: 'memechat',
+  redirectTo: '/p9/login',
+  directTo: 'p9-memechat.html',
 });
 
-p9Router.get("/memechat", renderMemechatPage);
-p9Router.post("/api/createMeme", handleMemeCreation);
+p9Router.get('/memechat', renderMemechatPage);
+p9Router.post('/api/createMeme', handleMemeCreation);
 
-p9Router.get("/login", renderLoginPage);
-p9Router.post("/api/login", handleLogin);
+p9Router.get('/login', renderLoginPage);
+p9Router.post('/api/login', handleLogin);
 
-p9Router.get("/api/meme/:userMeme", handleSendingMeme);
-p9Router.get("/api/memes", handleSendingAllMemes);
+p9Router.get('/api/meme/:userMeme', handleSendingMeme);
+p9Router.get('/api/memes', handleSendingAllMemes);
 
 export default p9Router;
--- routes/memegen.js
+++ routes/memegen.js
@@ -1,9 +1,9 @@
-import express from "express";
-import MemesGen from "../controllers/memegen-controller.js";
+import express from 'express';
+import MemesGen from '../controllers/memegen-controller.js';
 
 const memesGen = new MemesGen();
 const p3Router = express.Router();
 
-p3Router.get("/memegen/api/:text", memesGen.handleMemesGen);
+p3Router.get('/memegen/api/:text', memesGen.handleMemesGen);
 
 export default p3Router;
